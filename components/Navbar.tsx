9a0570fc1146546e816f490cb50f637dc4a97a15
alexsaussier <asaussier99@gmail.com>
2026-03-01 19:22:37 -0300

    fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar({ activePage = 'home' }: NavbarProps) {
             </Link>
           </div>
           <a href="/#get-started" className="bg-accent hover:bg-accent-dark text-white px-6 py-2.5 rounded-lg text-sm font-medium transition-colors">
-            Get Started
+            Get in touch
           </a>
         </div>
       </nav>
